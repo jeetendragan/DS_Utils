b30bdc2740206c3db0fd0461539e8d0cf2345a12
Jeetendra Gan <jeetendra.gan@gmail.com>
2020-01-04 15:58:22 -0500

    Add methods to drop columns and rows with missing values

--- cleanerUtils.py
+++ cleanerUtils.py
@@ -3,3 +3,13 @@ import pandas as pd
 def get_missing_valuecount_by_col(data):
     missing_val_count_by_column = (data.isnull().sum())
     return missing_val_count_by_column[missing_val_count_by_column > 0]
+
+def get_col_names_with_missing_values(dataframe):
+    return dataframe.columns[dataframe.isnull().any()].tolist()
+
+def drop_columns_with_missing_values(dataframe):
+    cols_with_missingvals = get_col_names_with_missing_values(dataframe)
+    return dataframe.drop(columns = cols_with_missingvals)
+
+def drop_rows_with_missing_values(dataframe):
+    return dataframe.dropna()
--- driver.py
+++ driver.py
@@ -2,9 +2,19 @@ import cleanerUtils
 import pandas as pd
 import numpy as np
 
-age = np.array([19, None, 25, 18, 19])
+age = np.array([12, 20, 25, 18, 19])
 gender = np.array(['Male', None, 'Female', None, 'Female'])
 df = pd.DataFrame({'age' : age, 'gender': gender})
 print(df)
 
 print(cleanerUtils.get_missing_valuecount_by_col(df))
+print("*****")
+print(cleanerUtils.get_col_names_with_missing_values(df))
+
+new_df = cleanerUtils.drop_columns_with_missing_values(df)
+print(new_df)
+
+new_df2 = cleanerUtils.drop_rows_with_missing_values(df)
+print(new_df2)
+
+
